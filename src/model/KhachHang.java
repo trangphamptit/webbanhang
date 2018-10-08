6b6313dc2d1aa17c439fb549556fa375a2cb8c2d
LaHyHy <en.pyrish.ym@gmail.com>
2018-10-08 17:11:41 +0700

    update

--- src/model/KhachHang.java
+++ src/model/KhachHang.java
@@ -1,5 +1,74 @@
 package model;
 
 public class KhachHang {
+	private int maKhachHang;
+	private String tenKhachHang;
+	private String ngaySinh;
+	private String diaChi;
+	private String soDienThoai;
+	private String email;
+	
+	public KhachHang(int maKhachHang, String tenKhachHang, String ngaySinh, String diaChi, String soDienThoai, String email) {
+		this.maKhachHang = maKhachHang;
+		this.tenKhachHang=tenKhachHang;
+		this.ngaySinh=ngaySinh;
+		this.diaChi=diaChi;
+		this.soDienThoai=soDienThoai;
+		this.email=email;
+	}
 
+
+	public KhachHang() {}
+	
+	public int getMaKhachHang() {
+		return maKhachHang;
+	}
+
+	public void setMaKhachHang(int maKhachHang) {
+		this.maKhachHang = maKhachHang;
+	}
+
+	public String getTenKhachHang() {
+		return tenKhachHang;
+	}
+
+	public void setTenKhachHang(String tenKhachHang) {
+		this.tenKhachHang = tenKhachHang;
+	}
+
+	public String getNgaySinh() {
+		return ngaySinh;
+	}
+
+	public void setNgaySinh(String ngaySinh) {
+		this.ngaySinh = ngaySinh;
+	}
+
+	public String getDiaChi() {
+		return diaChi;
+	}
+
+	public void setDiaChi(String diaChi) {
+		this.diaChi = diaChi;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSoDienThoai() {
+		return soDienThoai;
+	}
+
+	public void setSoDienThoai(String soDienThoai) {
+		this.soDienThoai = soDienThoai;
+	}
+	
+	public String toString() {
+		return "KhachHang [maKhachHang=" + maKhachHang + ", tenKhachHang=" + tenKhachHang + ", ngaySinh=" + ngaySinh + ", diaChi=" + diaChi +", soDienThoai=" + soDienThoai + ", email=" + email + "]";
+	}
 }
